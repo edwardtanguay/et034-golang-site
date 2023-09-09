0260d8f6cd8081f737bce5ab9ab08bd1cf3c48b8
Edward Tanguay <edward@tanguay.info>
2023-09-09 02:07:30 +0200

    Update gensite.go

--- gensite.go
+++ gensite.go
@@ -1,1 +1,30 @@
 // go script
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Define the file name
+	fileName := "example.txt"
+
+	// Create or open the file for writing, create it if it doesn't exist, truncate if it does
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Write content to the file
+	content := "Hello, this is some text written to the file!"
+	_, err = file.WriteString(content)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("File created and content written successfully.")
+}
